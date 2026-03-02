3c84f9d817d38204418771412f7798fad60e38ba
Alverino Stefano s354116 <s312200@studenti.polito.it>
2026-03-02 18:13:16 +0100

    feat: added comments in counter interface

--- src/it/polito/sda/examples/counter/CounterI.java
+++ src/it/polito/sda/examples/counter/CounterI.java
@@ -1,8 +1,27 @@
 package it.polito.sda.examples.counter;
 
 public interface CounterI {
+    
+    /**
+     * Increments the counter value by one.
+     */
     void increment();
+
+    /**
+     * Decrements the counter value by one.
+     * @return false if the decrementation can't be done otherwise true
+     */
     boolean decrement();
+
+    /**
+     * Resets the counter value to zero.
+     */
     void reset();
+
+    /**
+     * Returns the current value of the counter.
+     * 
+     * @return the current counter value
+     */
     int getValue();
 }
